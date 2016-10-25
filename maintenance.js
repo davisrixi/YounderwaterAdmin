6060e6fb0ee3fd568407825a10f224f4bbe87cb7
Davis Rixi <davis@Sleipnir.local>
2016-10-24 21:38:26 -0500

    Applying Angular URL Routing

--- maintenance.js
+++ maintenance.js
@@ -1,32 +1,17 @@
-angular.module('maintenance',[]).controller('adminCtrl',AdminCtrl);
+angular.module('maintenance',['ngRoute'])
+    .controller('adminCtrl',AdminCtrl)
+    .config(function($routeProvider){
+        $routeProvider.when('/locations',{
+            templateUrl:'views/locations.html'
+        });
+        $routeProvider.when('/sites',{
+            templateUrl:'views/sites.html'
+        });
+        $routeProvider.otherwise({
+            templateUrl:'views/main.html'
+        });
+});
 
 function AdminCtrl($scope){
-    $scope.activeMenu='';
-    $scope.isActive = isActive;
-    $scope.showMain = showMain;
-    $scope.showLocations = showLocations;
-    $scope.showDiveSites = showDiveSites;
 
-    $scope.showMain();
-
-    function setView(view,menuId){
-        $scope.view = view;
-        $scope.activeMenu - menuId;
-    }
-
-    function isActive(menuId){
-        return $scope.activeMenu = menuId;
-    }
-
-    function showMain(){
-        setView('main','');
-    }
-
-    function showLocations(){
-        setView('locations','Locations');
-    }
-
-    function showDiveSites(){
-        setView('diveSites','Sites');
-    }
 }
